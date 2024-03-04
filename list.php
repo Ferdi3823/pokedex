131c28a7f5cd3b44a54b40554e17ceb5df06d088
Ferdinand MONNAN <ferdi3823@gmail.com>
2024-03-04 15:58:12 +0100

    requete unique

--- list.php
+++ list.php
@@ -9,7 +9,11 @@
 -->
 <?php require_once("head.php"); 
 require_once('database-connection.php'); 
-$query = $databaseConnection->query("SELECT NomPokemon, IdPokemon, urlPhoto, T.libelleType AS 'Type 1', T2.libelleType AS 'Type 2' from pokemon P JOIN typepokemon T ON P.IdTypePokemon = T.IdType LEFT JOIN typepokemon T2 ON P.IdSecondTypePokemon = T2.IdType ORDER BY IdPokemon"); 
+$query = $databaseConnection->query("SELECT NomPokemon, IdPokemon, urlPhoto, T.libelleType AS 'Type 1', T2.libelleType AS 'Type 2' 
+    from pokemon P JOIN typepokemon T ON P.IdTypePokemon = T.IdType 
+    LEFT JOIN typepokemon T2 ON 
+    P.IdSecondTypePokemon = T2.IdType 
+    ORDER BY IdPokemon"); 
     if (!$query) { throw new RuntimeException("Cannot execute query. Cause: " . mysqli_error($databaseConnection)); } 
     else { 
         $pokemons = $query->fetch_all(MYSQLI_ASSOC); 
--- pokemon-unique.php
+++ pokemon-unique.php
@@ -2,17 +2,32 @@
 require_once('database-connection.php');
 $idPokemon = $_GET['id'];
 
-$query = $databaseConnection->query("SELECT NomPokemon, IdPokemon, urlPhoto, T.libelleType AS 'Type 1', T2.libelleType AS 'Type 2' 
-from pokemon P
- JOIN typepokemon T ON P.IdTypePokemon = T.IdType
-  LEFT JOIN typepokemon T2 ON P.IdSecondTypePokemon = T2.IdType 
-  WHERE idPokemon = $idPokemon
-  ORDER BY IdPokemon"); 
+$query = $databaseConnection->query("SELECT P.NomPokemon AS 'Nom', P.IdPokemon AS 'Id', P.urlPhoto AS 'Photo', P.PV AS 'PV', P.Attaque AS 'Attaque', P.Defense AS 'Defense', P.Vitesse AS 'Vitesse', P.Special AS 'Special', E.idEvolution AS 'Evolution',P2.NomPokemon AS 'Nom2', P2.IdPokemon AS 'Id2', P2.urlPhoto AS 'Photo2' 
+    from evolutionpokemon E 
+    JOIN pokemon P ON E.IdPokemon = P.idPokemon 
+    LEFT JOIN Pokemon P2 ON 
+    E.idEvolution = P2.IdPokemon 
+    WHERE P.IdPokemon = $idPokemon"); 
     if (!$query) { throw new RuntimeException("Cannot execute query. Cause: " . mysqli_error($databaseConnection)); } 
     else { 
         $idpokemon = $query->fetch_assoc();
-        echo "<h1> Pokemon n° " . $idpokemon['IdPokemon'] ."</h1>"
+        echo "<h1> Pokemon n° " . $idpokemon['Id'] ."</h1>
+        <h3>" . $idpokemon['Nom'] ."</h3>
+        <img src='" . $idpokemon['Photo']. "' alt='" . $idpokemon["Nom"] . "'>"
         ;
-    }
+        echo "<table>"; 
+        echo "<tr>
+        <th>PV</th>
+        <th>Defense</th>
+        <th>Vitesse</th>
+        <th>Attaque Spéciale</th>
+        </tr>";
+    echo "<tr>
+    <td>" . $idpokemon["PV"] . "</td>
+    <td>" . $idpokemon["Defense"] . "</td>
+    <td>" . $idpokemon["Vitesse"] . "</td>
+    <td>" . $idpokemon["Special"] . "</td>
+    </tr>"; } 
+    echo "</table>";
 require_once("footer.php");
 ?>
